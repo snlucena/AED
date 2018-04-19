a62acff15797dd445050226382fcbaeb53f7f646
snlucena <37759376+snlucena@users.noreply.github.com>
2018-04-19 00:03:54 -0300

    Subida ejemplotipos.cpp

---
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -0,0 +1,41 @@
+/* Ejemplos de tipos de datos
+ Sofia Lucena
+ 18042018
+ */
+
+#include <iostream>
+
+int a = 3, b = 7;
+bool comparar = a<b;
+
+int main () {
+
+	std::cout <<comparar;
+
+
+
+char letra = 'a';
+
+
+	std::cout << "La primera letra del abecedario es:" << letra;
+
+
+
+int x = 10, y = 3;
+unsigned z = x+y; 
+
+ std::cout << z;
+
+
+int p = -4, r = 1;
+
+ std::cout << p-r;
+
+
+double h = 43.5, d = 22.2;
+bool comparar = h<=d; 
+ 
+ std::cout << comparar;
+
+
+}
