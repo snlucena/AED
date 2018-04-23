cfa770bea4063db8bfbf12bf1da63a87915eb85b
snlucena <37759376+snlucena@users.noreply.github.com>
2018-04-22 21:29:37 -0300

    Agregado ejemplo string

---
+++ 02-EjemploTipos/string.cpp
@@ -0,0 +1,16 @@
+/* Sofia Lucena
+ 1569430
+ 22042018
+ */
+
+#include <iostream> 
+#include <string>
+#include <cassert>
+
+std:: string mensaje = "Feliz dia Tierra nuestra, ";
+std:: string nuevo = "brindo por tu inmensidad <3";
+
+int main () {
+
+	std:: cout << mensaje + nuevo; 
+} 
